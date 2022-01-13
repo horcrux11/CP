00a25d179f572dd69e7b2fcf4fb4da2ecffc441d
horcrux11 <72157309+horcrux11@users.noreply.github.com>
2022-01-13 17:12:47 +0530

    Add files via upload

---
+++ prog.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class value
+{
+    public Static void main[String args[]
+    {
+        Scanner in=new Scanner(System.in)
+        int u,a,t,v=0;
+        System.out.println("Enter a value of u");
+        u=in.nextInt();
+        System.out.println("Enter a value of a");
+        a=in.nextInt ();
+        System.out.println("Enter a value of t");
+        t=in.nextInt ();
+        v=u+a*t;
+         System.out.println(+v);
+    }
+}
+
